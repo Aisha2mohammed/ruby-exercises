9ffaedf281a547f7eccd8418bad7c503c004caaf
Aisha2mohammed <hayunadus111@gmail.com>
2024-10-30 20:53:23 +0300

    Test the exercise of ruby methods

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,31 +7,59 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(number)
+ number.chr  if    number.is_a?(Integer) && number.between?(0,127)
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+def common_sports(cur,val)
+ 
+result = cur & val
 
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#uniq and Array#sort together
+def alphabetical_list(games)
 
+games.uniq.sort
 
+end
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+def lucky_number(num = 7)
 
+"Today's lucky number is #{num}"
 
+end
 # method name: #ascii_code
 # parameter: character (a string)
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+# def ascii_code(letter)
 
+# if letter.is_a?(String) && letter.length? == 1 
+#     letter.ord 
+# else 
+#      'Input Error'
+# end
+# end 
+def ascii_code(letter)
+    if letter.is_a?(String) && letter.length == 1
+      letter.ord  # Get the ASCII code of the single character
+    else
+      'Input Error'
+    end
+  end
+  
 # method name: #pet_pun
 # parameter: animal (a string)
 # return value: nil
@@ -45,4 +73,7 @@
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
+def twenty_first_century?(year)
+     year >= 2001  &&  year <= 2100
 
+end    
