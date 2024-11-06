cfc19d00d250e2fc236180c63644ca5fed5a6e94
Aisha2mohammed <hayunadus111@gmail.com>
2024-11-06 23:29:24 +0300

    Test the Ruby exercises

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -1,19 +1,26 @@
 def coffee_drink?(drink_list)
-  # use #include? to return true when the drink_list (array) contains the string "coffee" or "espresso"
+  # Use #include? to check for "coffee" or "espresso" in the list
+  drink_list.include?("coffee") || drink_list.include?("espresso")
 end
 
 def correct_guess?(guess_list, answer)
-  # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+   guess_list.any? { |val|  val == answer} 
+  
 end
 
+  # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+
 def twenty_first_century_years?(year_list)
+  year_list.all?{|val| val >= 2001 &&  val <= 2100}
   # use #all? to return true when all of the years in the year_list (array) are between 2001 and 2100
 end
 
 def correct_format?(word_list)
-  # use #none? to return true when none of the words in the word_list (array) are in upcase
+  word_list.none? { |val| val == val.upcase }
 end
 
+
 def valid_scores?(score_list, perfect_score)
+  score_list.one?{|key, val| val == perfect_score}
   # use #one? to return true when only one value in the score_list (hash) is equal to the perfect_score (number)
 end
